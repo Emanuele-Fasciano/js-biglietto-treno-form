47ef16f4324c8bebef36b2945a78e58901cc878b
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-01-25 18:46:25 +0100

    end exercise

--- js/main.js
+++ js/main.js
@@ -1,48 +1,60 @@
+const inputButtonEl = document.getElementById("calculate_tiket");
+const userNameEl = document.getElementById("name_and_surname");
+const userAgeEl = document.getElementById("age");
+const userKmEl = document.getElementById("km");
+const coachNumberEl = document.getElementById("coach_number")
+const outputNameUser = document.getElementById("name_printed")
+const outputTiketPrice = document.getElementById("tiketPrice")
+const outoutRandomCode = document.getElementById("random_code")
 
-// const userKm = parseInt(prompt(`Quanti KM devi percorrere?`));
-// const pricePerKm = 0.21;
 
-
-const userAgeText = document.getElementById("userAge")
-// console.log(userAgeText);
-
-userAgeText.addEventListener(
-    "click",
+inputButtonEl.addEventListener(
+    'click',
 
     function () {
-        const userAge = parseInt(`Quanti anni hai?`);
+        let outputMessage;
+        const userName = userNameEl.value;
+        const userAge = userAgeEl.value;
+        const userKm = userKmEl.value;
+        const pricePerKm = 0.21;
+        let randomCodeGen = Math.floor(Math.random() * 10000) + 1
+        let randomCoachGen = Math.floor(Math.random() * 50) + 1
+
+        if (!isNaN(userAge) && !isNaN(userKm)) {
+            let price = pricePerKm * userKm;
+
+            if (userAge < 18) {
+                const discountPercUnder = 20
+                const discountAmountUnder = (price * discountPercUnder) / 100
+                price -= discountAmountUnder
+                outputMessage = `€${price.toFixed(2)}`
+                // console.log(outputMessage);
+            }
+            else if (userAge > 64) {
+                const discountPercOver = 40;
+                const discountAmountOver = (price * discountPercOver) / 100
+                price -= discountAmountOver
+                outputMessage = `€${price.toFixed(2)}`
+            }
+            else {
+                outputMessage = `€${price.toFixed(2)}`
+            }
+        }
+        else {
+            outputMessage = `inserire dati corretti`
+        }
+
+        // output
+        outputNameUser.innerHTML = userNameEl.value
+        outputTiketPrice.innerHTML = outputMessage
+        outoutRandomCode.innerHTML = randomCodeGen
+        coachNumberEl.innerHTML = randomCoachGen
+
+
     }
+
+
 )
 
 
-let outputMessage;
 
-if (!isNaN(userAge) && !isNaN(userKm)) {
-    let price = userKm * pricePerKm;
-
-    if (userAge < 18) {
-        const discountPercUnder = 20
-        const discountAmountUnder = (price * discountPercUnder) / 100
-        price -= discountAmountUnder
-        outputMessage = `Il prezzo per i minorenni é ${price} `
-        // console.log(outputMessage);
-    }
-
-    else if (userAge > 64) {
-        const discountPercOver = 40
-        const discountAmountOver = (price * discountPercOver) / 100
-        price -= discountAmountOver
-        outputMessage = `Il prezzo per gli over 64 é ${price} `
-        // console.log(outputMessage);
-    }
-
-    else {
-        outputMessage = `Il prezzo del biglietto intero è ${price}`
-        // console.log(outputMessage);
-    }
-
-    // alert(`${outputMessage}£`)
-}
-else {
-    // alert(`Inserire dei valori corretti e ricaricare`)
-}
